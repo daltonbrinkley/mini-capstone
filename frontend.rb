c6b09cf240ac05764aea488dd1dd2b09229e321b
daltonbrinkley <dbrinkles@gmail.com>
2018-03-06 20:03:07 -0600

    added table gem

--- frontend.rb
+++ frontend.rb
@@ -1,4 +1,5 @@
 require "unirest"
+require "tty-table"
 
 system "clear"
 
@@ -12,7 +13,23 @@ if input_option == "1"
   product = response.body
   p product
 elsif input_option == "2"
-  response = Unirest.get("http://localhost:3000/show_all_products_url""http://localhost:3000/second_contact_url")
+  response = Unirest.get("http://localhost:3000/show_all_products_url")
   products = response.body
   p products
 end
+
+  table_header = ['Name:','Price:','Image URL:','Description:']
+  
+  # products.length.times do
+  #   index = 0
+
+  table_body = [[products[0]["name"], products[0]["price"], products[0]["image_url"], products[0]["description"]]]
+#     index = index + 1
+# end
+
+  table = TTY::Table.new table_header, table_body
+  puts table.render(:ascii)
+
+
+# table = TTY::Table.new ['Name:','Price:','Image URL:','Description:'], [[Product.first[name:], 'a2'], ['b1', 'b2']]
+# p table.render(:ascii)
