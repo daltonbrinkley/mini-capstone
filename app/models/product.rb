b6e23cf8b495b2dc824b58ddee37d3d9484a2eda
daltonbrinkley <dbrinkles@gmail.com>
2018-03-13 18:40:24 -0500

    changed description and price attributes to correct data types

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,30 @@
 class Product < ApplicationRecord
+  def is_discounted
+    if price < 2
+      return "True"
+    else
+      return "False"
+    end
+  end
+
+  def tax
+    return price*0.09
+  end
+
+  def total
+    return price+tax
+  end
+
+  def as_json
+    {
+      id: id,
+      name: name,
+      price: price,
+      tax: tax,
+      total: total,
+      image_url: image_url,
+      description: description,
+      is_discounted: is_discounted
+    }
+  end
 end
---
+++ db/migrate/20180313232844_change_description_to_string.rb
@@ -0,0 +1,5 @@
+class ChangeDescriptionToString < ActiveRecord::Migration[5.1]
+  def change
+    change_column :products, :description, :text
+  end
+end
