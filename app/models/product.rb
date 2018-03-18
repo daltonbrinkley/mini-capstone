9d17c4578198c8a5393d787893b416341e35432b
daltonbrinkley <dbrinkles@gmail.com>
2018-03-18 10:12:42 -0500

    added supplier model with supplier_id link to product model and Add model method associations between Product and Supplier

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,10 @@ class Product < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :description, length: { in: 10..500 }
 
+  def supplier
+    supplier = Supplier.find_by(id: supplier_id)
+  end
+
   def is_discounted
     if price < 2
       return "True"
@@ -29,7 +33,8 @@ class Product < ApplicationRecord
       total: total,
       image_url: image_url,
       description: description,
-      is_discounted: is_discounted
+      is_discounted: is_discounted,
+      supplier: supplier.as_json
     }
   end
 end
