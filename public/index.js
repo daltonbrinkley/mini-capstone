64f8f402356655dd882873a45f2f631a95de7722
daltonbrinkley <dbrinkles@gmail.com>
2018-04-08 10:10:22 -0500

    Add static HTML frontend and Use JavaScript to paste template multiple times

---
+++ public/index.js
@@ -0,0 +1,10 @@
+var productTemplate = document.querySelector("#product-row");
+var productContainer = document.querySelector(".row");
+
+productContainer.appendChild(productTemplate.content.cloneNode(true));
+productContainer.appendChild(productTemplate.content.cloneNode(true));
+productContainer.appendChild(productTemplate.content.cloneNode(true));
+productContainer.appendChild(productTemplate.content.cloneNode(true));
+productContainer.appendChild(productTemplate.content.cloneNode(true));
+productContainer.appendChild(productTemplate.content.cloneNode(true));
+productContainer.appendChild(productTemplate.content.cloneNode(true));
