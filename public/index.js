6fb65dbb769220780981617879a6c43995904b7d
daltonbrinkley <dbrinkles@gmail.com>
2018-04-15 11:09:13 -0500

    Updated HTML homepage, connected with database to pull data about products and display, added login, logout, and a sample page with routes and links in the HTML

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
-var productTemplate = document.querySelector("#product-card");
-var productContainer = document.querySelector(".row");
+// var productTemplate = document.querySelector("#product-card");
+// var productContainer = document.querySelector(".row");
 
 // productContainer.appendChild(productTemplate.content.cloneNode(true));
 // productContainer.appendChild(productTemplate.content.cloneNode(true));
@@ -8,16 +8,150 @@ var productContainer = document.querySelector(".row");
 // productContainer.appendChild(productTemplate.content.cloneNode(true));
 // productContainer.appendChild(productTemplate.content.cloneNode(true));
 
-axios.get("http://localhost:3000/v1/products").then(function(response) {
-  var products = response.data;
-  console.log(products);
+// axios.get("http://localhost:3000/v1/products").then(function(response) {
+//   var products = response.data;
+//   console.log(products);
 
-  products.forEach(function(product) {
-    var productClone = productTemplate.content.cloneNode(true);
-    productClone.querySelector(".card-title").innerText = product.name;
-    productClone.querySelector(".price").innerText = product.price;
-    productClone.querySelector(".description").innerText = product.description;
-    productClone.querySelector(".card-img-top").src = product.image;
-    productContainer.appendChild(productClone);
-  });
+//   products.forEach(function(product) {
+//     var productClone = productTemplate.content.cloneNode(true);
+//     productClone.querySelector(".card-title").innerText = product.name;
+//     productClone.querySelector(".price").innerText = product.price;
+//     productClone.querySelector(".description").innerText = product.description;
+//     productClone.querySelector(".card-img-top").src = product.image;
+//     productContainer.appendChild(productClone);
+//   });
+// });
+
+
+
+/* global Vue, VueRouter, axios */
+
+var HomePage = {
+  template: "#home-page",
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      products: []
+    };
+  },
+  created: function() {
+    axios.get("v1/products").then(function(response) { 
+      this.products = response.data;
+      console.log(this.products);
+    }.bind(this)
+    );
+  },
+  methods: {},
+  computed: {}
+};
+
+var SamplePage = {
+  template: "#sample-page",
+  data: function() {
+    return {
+      message: "Welcome to the Sample Page!!!"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
+var SignupPage = {
+  template: "#signup-page",
+  data: function() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.passwordConfirmation
+      };
+      axios
+        .post("/v1/users", params)
+        .then(function(response) {
+          router.push("/login");
+        })
+        .catch(
+          function(error) {
+            this.errors = error.response.data.errors;
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var LoginPage = {
+  template: "#login-page",
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        auth: { email: this.email, password: this.password }
+      };
+      axios
+        .post("/user_token", params)
+        .then(function(response) {
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          router.push("/");
+        })
+        .catch(
+          function(error) {
+            this.errors = ["Invalid email or password."];
+            this.email = "";
+            this.password = "";
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var LogoutPage = {
+  template: "<h1>Logout</h1>",
+  created: function() {
+    axios.defaults.headers.common["Authorization"] = undefined;
+    localStorage.removeItem("jwt");
+    router.push("/");
+  }
+};
+
+
+var router = new VueRouter({
+  routes: [{ path: "/", component: HomePage },
+    { path: "/sample", component: SamplePage },
+    { path: "/signup", component: SignupPage },
+    { path: "/login", component: LoginPage },
+    { path: "/logout", component: LogoutPage },
+  ],
+  scrollBehavior: function(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  }
+});
+
+var app = new Vue({
+  el: "#vue-app",
+  router: router,
+  created: function() {
+    var jwt = localStorage.getItem("jwt");
+    if (jwt) {
+      axios.defaults.headers.common["Authorization"] = jwt;
+    }
+  }
 });
